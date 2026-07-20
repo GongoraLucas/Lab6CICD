335da6f6d832aa95073d73821900c97e0ac83a6c
GongoraLucas <gongoralucas59@gmail.com>
2026-07-20 16:19:06 -0500

    Cambio nombre variables

--- sum.js
+++ sum.js
@@ -1,11 +1,11 @@
 /**
  * Función que recibe dos números y retorna la suma de ambos.
- * @param {number} a - Primer número a sumar.
- * @param {number} b - Segundo número a sumar.
+ * @param {number} sum1 - Primer número a sumar.
+ * @param {number} sum2 - Segundo número a sumar.
  * @returns {number} Resultado de la suma.
  */
-function sum(a, b) {
-  return a + b;
+function sum(sum1, sum2) {
+  return sum1 + sum2;
 }
 
 export default sum;
